f09e40d0e004690deb1ec635d6a1763e890e84ff
Gustavo Dev <gustavopicolitona@gmail.com>
2024-02-27 10:46:40 -0300

    New solution / 27.02

---
+++ 1018.py
@@ -0,0 +1,14 @@
+# Lendo o valor inteiro
+valor = int(input())
+
+# Lista das notas disponíveis em ordem decrescente
+notas = [100, 50, 20, 10, 5, 2, 1]
+
+# Imprimindo o valor lido
+print(valor)
+
+# Iterando sobre as notas para contar quantas são necessárias
+for nota in notas:
+    qtd_notas = valor // nota
+    print(f"{qtd_notas} nota(s) de R$ {nota},00")
+    valor -= qtd_notas * nota
