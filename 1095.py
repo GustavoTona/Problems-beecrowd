69eeebfb3e250963e45939fed80d200f3d7c4d74
Gustavo Dev <gustavopicolitona@gmail.com>
2024-03-05 15:44:49 -0300

    New solution / 05.03

---
+++ 1095.py
@@ -0,0 +1,10 @@
+
+# Valores iniciais para padronizar o I e J
+I = 1
+J = 60
+
+# While para enquanto o J for maior ou igual a 0 
+while J >= 0:
+    print(f'I={I} J={J}')
+    I += 3 #incrementando  ovalor
+    J -= 5 # descrementando o valor 
